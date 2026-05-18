7eddccb601e4afcbdb9bba4c081b2d1ed4937d3f
Eshanto Ghop Apurbo <eshantoghopapurbo@gmail.com>
2026-05-18 08:46:13 +0600

    study server project

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const dontenv =require("dotenv")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dontenv.config()
+
+const uri = process.env.MONGODB_URL;
+const app = express()
+const port = process.env.PORT||5000
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('server running is fine')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
