6050e1e8f16b6ea47aaaea8ea85785e0961f2dad
Eshanto Ghop Apurbo <eshantoghopapurbo@gmail.com>
2026-05-19 17:58:37 +0600

    edit update

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ async function run() {
         res.json(result)
     })
 
+    app.patch("/rooms/:id",async (req,res)=> {
+      const {id} =req.params
+      const updatedData =req.body
+
+      const result = await addroomCollection.updateOne(
+        {_id: new ObjectId(id)},
+         {$set:updatedData}
+      )
+      res.json(result) 
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
